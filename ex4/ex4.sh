cc133dc21b9a8974b915e62ed8f9ce261a285e26
Angel <angel@x8bit.com>
2018-06-30 13:45:05 -0500

    Fix ex4

--- ex4/ex4.sh
+++ ex4/ex4.sh
@@ -9,6 +9,13 @@ usage() {
 main() {
     local title=$1
     local artist=$2
+    if [[ -z "$title" ]]; then
+        echo "Ingrese título de la canción"
+        exit 1
+    elif [[ -z "$artist" ]]; then
+        echo "Ingrese artista"
+        exit 1
+    fi
     local url="http://www.metrolyrics.com/$title-lyrics-$artist.html"
     
     res=$(curl $url | pup --color --plain "#lyrics-body-text p text{}")
